87a8c47af1d2af8438995ec3b3f27251ecdaeb3f
Zeeshan17CO50 <mohammedsiddiquezee@gmail.com>
2023-09-27 20:41:12 +0530

    Refractor: remove duplicated styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,10 +2,10 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react'
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="300px" borderRadius={10} overflow={'hidden'}>
-        <Skeleton height='200px'></Skeleton>
+    <Card>
+        <Skeleton height='200px' />
         <CardBody>
-            <SkeletonText></SkeletonText>
+            <SkeletonText />
         </CardBody>
     </Card>
   )
